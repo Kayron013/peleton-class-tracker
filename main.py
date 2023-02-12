a2523550a514def6a33878b79d7c63fad3ef3cf9
Angel Campbell <kayron013@gmail.com>
2023-02-11 16:14:57 -0800

    get class date and reserve link; filter for non full/waitlistable classes

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+from typing import List
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support.wait import WebDriverWait
+import pprint
 
 PELETON_SCHEDULE_URL = 'https://studio.onepeloton.com/new-york/schedule'
+
+pp = pprint.PrettyPrinter(indent=4)
 
 
 def main():
@@ -12,16 +17,25 @@ def main():
     driver = webdriver.Chrome(options=chrome_options)
 
     driver.get(PELETON_SCHEDULE_URL)
-    driver.implicitly_wait(1)
 
     title = driver.title
-    class_cards = driver.find_elements(
-        By.CSS_SELECTOR, '[data-test-class=classCards]')
     print(title)
-    print(get_card_details(class_cards[0]))
+
+    class_cards: List[WebElement] = WebDriverWait(driver, 10).until(lambda driver: driver.find_elements(By.CSS_SELECTOR, '[data-test-class=classCards]'))
+    (first_class, last_class) = (get_card_details(class_cards[0]), get_card_details(class_cards[-1]))
+    print('{} classes from {} to {}'.format(len(class_cards), first_class['date'], last_class['date']))
+
+    filtered_class_cards = list(filter(lambda x: 'CLASS FULL' not in x.text, class_cards))
+    classes = [get_card_details(card) for card in filtered_class_cards]
+
+    print('{} classes not full'.format(len(classes)))
+    pp.pprint(classes)
 
 
 def get_card_details(class_card: WebElement):
+    status = class_card.find_element(By.CSS_SELECTOR, ':nth-child(4)').text
+    is_full = status == 'CLASS FULL'
+
     return {
         'name': class_card.find_element(By.CSS_SELECTOR, '[data-test-id=className]').text,
         'instructor': {
@@ -29,9 +43,11 @@ def get_card_details(class_card: WebElement):
             'image': class_card.find_element(By.TAG_NAME, 'img').get_attribute('src')
         },
         'fitness_disciplines': class_card.find_element(By.CSS_SELECTOR, '[aria-label="Fitness Disciplines"]').get_attribute('data-test-info').split(','),
-        'status': class_card.find_element(By.CSS_SELECTOR, ':nth-child(4)').text,
+        'status': status,
         'time': class_card.find_element(By.CSS_SELECTOR, ':first-child').text,
-        'date': class_card.find_element(By.XPATH, '../../*[1]').text
+        # need xpath to get to parent elements
+        'date': class_card.find_element(By.XPATH, '../../*[1]' if is_full else '../../../*[1]').text,
+        'reserve_link': None if is_full else class_card.get_attribute('href')
     }
 
 
